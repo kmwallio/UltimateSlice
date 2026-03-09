08134b439d9ffa196cdb478af3ceff3221ab1668
kmwallio <mwallio@gmail.com>
2026-03-08 22:45:50 -0700

    add support for open file

--- src/app.rs
+++ src/app.rs
@@ -11,11 +11,10 @@ pub fn run(mcp_enabled: bool, startup_project_path: Option<String>) {
     // When running as an MCP server we've already terminated any prior instance
     // via the PID file. Use NON_UNIQUE so GTK doesn't block on D-Bus registration
     // while the old instance's session is still being cleaned up.
-    let flags = if mcp_enabled {
-        gio::ApplicationFlags::NON_UNIQUE
-    } else {
-        gio::ApplicationFlags::empty()
-    };
+    let mut flags = gio::ApplicationFlags::HANDLES_OPEN;
+    if mcp_enabled {
+        flags |= gio::ApplicationFlags::NON_UNIQUE;
+    }
 
     let app = Application::builder()
         .application_id(APP_ID)
@@ -27,8 +26,17 @@ pub fn run(mcp_enabled: bool, startup_project_path: Option<String>) {
     });
 
     let startup_project_path = Rc::new(RefCell::new(startup_project_path));
+    let startup_project_path_open = startup_project_path.clone();
     app.connect_activate(move |app| {
         let path = startup_project_path.borrow_mut().take();
+        build_window(app, mcp_enabled, path);
+    });
+    app.connect_open(move |app, files, _hint| {
+        let path = files
+            .first()
+            .and_then(|f| f.path())
+            .map(|p| p.to_string_lossy().into_owned())
+            .or_else(|| startup_project_path_open.borrow_mut().take());
         build_window(app, mcp_enabled, path);
     });
 
